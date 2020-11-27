7632175024ad2fd6fd8b960a5dde966c2254f0fc
fjij <30779570+fjij@users.noreply.github.com>
2020-11-26 23:43:59 -0500

    Add cors and helmet

--- web/app.js
+++ web/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mountRoutes = require('./routes');
 
 const app = express();
+app.use(helmet());
+app.use(cors());
 app.use(express.static(__dirname + '/public'));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(bodyParser.urlencoded({ extended: false }));
